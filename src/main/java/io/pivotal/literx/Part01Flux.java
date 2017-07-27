2b81eb407e72ca544b58c0c2d682ef0a4ad306bd
Ruddy BONDEL <rbondel.ext@galerieslafayette.com>
2017-07-27 18:24:15 +0200

    Complete Part01

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -1,6 +1,14 @@
 package io.pivotal.literx;
 
+import org.assertj.core.util.Lists;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 /**
  * Learn how to create Flux instances.
@@ -12,37 +20,39 @@ public class Part01Flux {
 
 //========================================================================================
 
-	// TODO Return an empty Flux
-	Flux<String> emptyFlux() {
-		return null;
-	}
+    // TODO Return an empty Flux
+    Flux<String> emptyFlux() {
+        return Flux.empty();
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
-	Flux<String> fooBarFluxFromValues() {
-		return null;
-	}
+    // TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+    Flux<String> fooBarFluxFromValues() {
+        return Flux.fromStream(Stream.of("foo", "bar"));
+    }
 
 //========================================================================================
 
-	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
-	Flux<String> fooBarFluxFromList() {
-		return null;
-	}
+    // TODO Create a Flux from a List that contains 2 values "foo" and "bar"
+    Flux<String> fooBarFluxFromList() {
+        return Flux.fromIterable(Lists.newArrayList("foo", "bar"));
+    }
 
 //========================================================================================
 
-	// TODO Create a Flux that emits an IllegalStateException
-	Flux<String> errorFlux() {
-		return null;
-	}
+    // TODO Create a Flux that emits an IllegalStateException
+    Flux<String> errorFlux() {
+        final Flux<String> stringFlux = fooBarFluxFromValues();
+        stringFlux.toIterable().forEach(String::toUpperCase);
+        return stringFlux;
+    }
 
 //========================================================================================
 
-		// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
-	Flux<Long> counter() {
-		return null;
-	}
+    // TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+    Flux<Long> counter() {
+        return Flux.fromStream(Stream.of(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L));
+    }
 
 }
